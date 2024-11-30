feffd2a602b69c2d2737f2b49eb608b81a937491
villalva <mstefanovg@gmail.com>
2024-11-30 14:53:41 -0500

    first commit

---
+++ app/src/main/java/com/villalva/stefano/laboratoriocalificadosustitoriovillalva/ApiService.kt
@@ -0,0 +1,9 @@
+package com.villalva.stefano.laboratoriocalificadosustitoriovillalva
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ApiService {
+    @GET("posts")
+    fun getPosts(): Call<List<Post>>
+}
---
+++ app/src/main/java/com/villalva/stefano/laboratoriocalificadosustitoriovillalva/Post.kt
@@ -0,0 +1,8 @@
+package com.villalva.stefano.laboratoriocalificadosustitoriovillalva
+
+data class Post(
+    val userId: Int,
+    val id: Int,
+    val title: String,
+    val body: String
+)
